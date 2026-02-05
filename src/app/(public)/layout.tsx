c840c8e5efddffc8845a75974a6bcd9626079c5c
Jhody Pedia <53709496+4rsl3G@users.noreply.github.com>
2026-02-05 17:08:06 +0700

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,9 +3,15 @@ import PublicFooter from "@/components/layout/PublicFooter";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-dvh flex flex-col">
+    <div className="min-h-dvh flex flex-col bg-[rgb(var(--bg))] text-[rgb(var(--text))]">
       <PublicHeader />
-      <main className="flex-1">{children}</main>
+
+      {/* penting: main juga kasih background biar gak ketiban style lain */}
+      <main className="flex-1 bg-[rgb(var(--bg))]">
+        {/* opsional: kalau kamu pakai bg-noise */}
+        <div className="bg-noise min-h-full">{children}</div>
+      </main>
+
       <PublicFooter />
     </div>
   );
